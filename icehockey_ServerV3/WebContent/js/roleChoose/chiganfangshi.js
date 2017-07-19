b842c9fee78b55c89872e22e70511da80ab27acd
Administrator <Administrator@QH-20170310NBUO>
2017-07-19 10:59:53 +0800

    roleChoose

---
+++ icehockey_ServerV3/WebContent/js/roleChoose/chiganfangshi.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+$(function(){
+    var sportEvent=null;
+    $('#left').on('click',function(){
+        sportEvent = "LEFT";
+        submit(sportEvent)
+    });
+    $('#right').on('click',function(){
+        sportEvent = "RIGHT";
+        submit(sportEvent)
+    });
+    //提交用户选择的信息
+    var urlUserId=comm.getUrlParameter("userid");
+    function submit(sportEvent){
+        var r=confirm("确认要提交么");
+        if (r==true)
+        {  
+            var data = {
+                handlingId : sportEvent,
+                userid:urlUserId
+            };
+
+            alert(JSON.stringify(data));
+            //TODO 请求后台保存数据
+            $.getJSON(HANDLINGURL,data,function(result){
+                //TODO 处理后台返回的结果
+                if(result.result!=0){
+                    //提交成功之后进行的操作
+                    window.location.href = '../bxy/gender.html';//接下来是性别选择
+                }
+            });
+        }
+        else
+        {}
+    }
+});
---
+++ icehockey_ServerV3/WebContent/js/roleChoose/rool.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+$(function(){
+
+    var sportEvent = null;
+
+    $("#qiuyuan").click(function(){
+        //每个按钮执行的操作
+        sportEvent = "QY";
+        submit(sportEvent)
+    });
+
+    $("#jiaolian").click(function(){
+        sportEvent = "JL";
+        submit(sportEvent)
+    });
+
+    $("#shoumen").click(function(){
+        sportEvent = "SM";
+        submit(sportEvent)
+    });
+    $("#caipan").click(function(){
+        sportEvent = "CP";
+        submit(sportEvent)
+    });
+    $("#school").click(function(){
+    	window.location.href = "school.html";
+       // sportEvent = "DS";
+       // submit(sportEvent)
+    });
+    $("#xiehui").click(function(){
+       // sportEvent = "DS";
+       // submit(sportEvent)
+       window.location.href = "team.html";
+    });
+
+
+    //提交用户选择的信息
+    var urlUserId=comm.getUrlParameter("userid");
+    function submit(sportEvent){
+        var r=confirm("确认要提交么");
+        if (r==true)
+        {
+            var data = {
+                role : sportEvent,
+                userid:urlUserId
+            };
+
+            alert(JSON.stringify(data));
+            //TODO 请求后台保存数据
+            $.getJSON(SNOWURL,data,function(result){
+                //TODO 处理后台返回的结果
+                if(result.result!=0){
+                    //提交成功之后进行的操作
+                    window.location.href = 'chiganfangshi.html';
+                }
+            });
+        }
+        else
+        {
+
+        }
+
+
+
+    }
+});
