9b18e8c6f5eb81d346c2b6b434ceee5050bb9e5f
25832 <25832@DESKTOP-Q8KVNF3>
2017-07-25 17:08:46 +0800

    linlin

---
+++ icehockey_ServerV3/WebContent/js/pageJS/imformation.js
@@ -0,0 +1,162 @@
+
+//$(document).ready(function(){
+//	  $(".button_Myself").click(function(){
+//	    var urlUserId=comm.getUrlParameter("userid");//解析url中的参数获取userid的值
+//		var url="../../demo_test.asp";
+////		var data={
+////		userid : urlUserId,
+////		weChatId : weChatId,
+////		telephone : telephone,
+////		userName : 李雷,
+////		sex : sex,
+////		birthday : birthday,
+////		country : country,
+////		city : city,
+////		height : height,
+////		weight : weight,
+////		play : play,
+////		ice_play : ice_play,
+////		snow_play : snow_play,
+////		roleId : roleId,
+////		handlingId : handlingId,
+////	  };
+//	  	$get(url,function(data,status){
+//		alert("Data: " + data + "\nStatus: " + status);
+//	});
+//	});
+//	})
+//$(document).ready(function(){
+//$(".button_Myself").click(function(){
+//	var data={
+//		userid : "urlUserId",
+//		weChatId : "weChatId",
+//		telephone : "telephone",
+//		userName : "userName",
+//		sex : "sex",
+//		birthday : "birthday",
+//		country : "country",
+//		city : "city",
+//		height : "height",
+//		weight : "weight",
+//		play : "play",
+//		ice_play : "ice_play",
+//		snow_play : "snow_play",
+//		roleId : "roleId",
+//		handlingId : "handlingId",
+//	  };
+//  $.post("../demo_test.asp",data,function(data,status){
+//  	
+////	  document.getElementById("userName").innerHTML=data.userName;
+//    alert("数据：" + data + "\n状态：" + status);
+//  });
+//});
+//});
+$(document).ready(function(){
+  $(".button_Myself").click(function(){
+	var urlUserId=comm.getUrlParameter("userid");//解析url中的参数获取userid的值
+	var weChatId=comm.getUrlParameter("wechatid");
+	var telephone=comm.getUrlParameter("telephone");
+	var userName=comm.getUrlParameter("username");
+		var birthday=comm.getUrlParameter("birthday");
+	var sex=comm.getUrlParameter("sex");
+	var country=comm.getUrlParameter("country");
+	var city=comm.getUrlParameter("city");
+	var height=comm.getUrlParameter("height");
+	var weight=comm.getUrlParameter("weight");
+	var play=comm.getUrlParameter("play");
+	var ice_play=comm.getUrlParameter("ice_play");
+	var snow_play=comm.getUrlParameter("snow_play");
+	var roleId=comm.getUrlParameter("roleId");
+	var handlingId=comm.getUrlParameter("handlingId");
+	
+
+	
+	var dataurl="";
+  	var data={
+  	userid : urlUserId,
+		weChatId : weChatId,
+		telephone : telephone,
+		userName : "张杰",
+		sex : sex,
+		birthday : "123456",
+		country : "中国",
+		city : "香港",
+		height : "175CM",
+		weight : "75KG",
+		play : play,
+		ice_play : ice_play,
+		snow_play : snow_play,
+		roleId : roleId,
+		handlingId : handlingId
+  	};
+    $.post(dataurl,data,function(data,status){
+      alert("数据：" + data + "\n状态：" + status);
+    });
+    document.getElementById("userName").innerHTML=data.userName;
+    document.getElementById("userbody").innerHTML=data.height+"/"+data.weight;
+    document.getElementById("usercountry").innerHTML=data.country;
+    
+  });
+  $(".button_next1").click(function(){
+		    window.location.href="../views/essentiainfo.html";});
+});
+$(document).ready(function(){
+  
+
+	var urlUserId=comm.getUrlParameter("userid");//解析url中的参数获取userid的值
+	var weChatId=comm.getUrlParameter("wechatid");
+	var telephone=comm.getUrlParameter("telephone");
+	var userName=comm.getUrlParameter("username");
+		var birthday=comm.getUrlParameter("birthday");
+	var sex=comm.getUrlParameter("sex");
+	var country=comm.getUrlParameter("country");
+	var city=comm.getUrlParameter("city");
+	var height=comm.getUrlParameter("height");
+	var weight=comm.getUrlParameter("weight");
+	var play=comm.getUrlParameter("play");
+	var ice_play=comm.getUrlParameter("ice_play");
+	var snow_play=comm.getUrlParameter("snow_play");
+	var roleId=comm.getUrlParameter("roleId");
+	var handlingId=comm.getUrlParameter("handlingId");
+	var dataurl="";
+  	var data={
+  	userid : urlUserId,
+		weChatId : weChatId,
+		telephone : telephone,
+		userName : "张杰",
+		sex : sex,
+		birthday : "123456",
+		country : "中国",
+		city : "香港",
+		height : "175CM",
+		weight : "75KG",
+		play : play,
+		ice_play : ice_play,
+		snow_play : snow_play,
+		roleId : roleId,
+		handlingId : handlingId
+  	};
+    $.post(dataurl,data,function(data,status){
+      alert("数据：" + data + "\n状态：" + status);
+    });
+    document.getElementById("userName1").innerHTML=data.userName;
+    document.getElementById("userbody1").innerHTML=data.height+"/"+data.weight;
+    document.getElementById("usercountry1").innerHTML=data.country;
+        document.getElementById("usersex1").innerHTML=data.sex;
+            document.getElementById("birthday1").innerHTML=data.birthday;
+                document.getElementById("usercountry1").innerHTML=data.country;
+                    document.getElementById("usercity1").innerHTML=data.city;
+                        document.getElementById("userplay1").innerHTML=data.play;
+                            document.getElementById("userice_play1").innerHTML=data.ice_play;
+                                document.getElementById("usersnow_play1").innerHTML=data.snow_play;
+                                    document.getElementById("userroleId1").innerHTML=data.roleId;
+                                        document.getElementById("userhandlingId1").innerHTML=data.handlingId;
+
+
+
+
+
+
+
+
+ });
